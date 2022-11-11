778a5816fa5c1348dd2fb0f97882ab870626c765
Bhuwanarya <aryabhuwan873@gmail.com>
2022-11-10 20:20:17 -0800

    Add:Leap year or not

--- Basic.java
+++ Basic.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int year;
+        System.out.println("Enter an Year :: ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+  
+        if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+           System.out.println("Specified year is a leap year");
+        else
+           System.out.println("Specified year is not a leap year");
     }
 }
