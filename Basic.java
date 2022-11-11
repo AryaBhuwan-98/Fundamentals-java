afafad6f34c10828862474d6f2d61c68e06742b0
Bhuwanarya <aryabhuwan873@gmail.com>
2022-11-10 20:30:04 -0800

    Add: Command Line argument

--- Basic.java
+++ Basic.java
@@ -1,5 +1,9 @@
 public class Basic {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("This is a simple command line program!");
+        System.out.println("Your Command Line arguments are:"); // loop through all arguments and print it to the user 
+        for (String str: args) {
+          System.out.println(str);
+        }
     }
 }
