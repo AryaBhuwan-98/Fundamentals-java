6cbaaecdfdb4a1ad2c9b54ecce67e2491ece7078
Bhuwanarya <aryabhuwan873@gmail.com>
2022-11-10 20:25:27 -0800

    Add: Static demo

--- Basic.java
+++ Basic.java
@@ -1,5 +1,18 @@
 public class Basic {
+     // A STATIC VARIABLE
+     static int a=42;
+
+     // A STATIC METHOD
+     static int Method(){
+        System.out.println("Value of a is"+a);
+        return a;
+     }
+      
+     // A STATIC BLOCK
+     static{
+        System.out.println("Static block initialized.");
+     }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(Method());
     }
 }
